d0aba7512c196580f39b0561c0940464189f4287
chia hung <a0926223478@hotmail.com>
2021-06-21 21:40:42 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+
+const usePassport = require('./config/passport')
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
 
+usePassport(app)
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMysecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extend: true}))
 app.use(methodOverride(')method'))
-
-app.get('/', (req,res) =>{
-  res.send('hello world')
-})
+app.use(routes)
 
 app.listen(PORT, () =>{
   console.log(`app is running on http://localhost:${PORT}`)
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+const users = require('./modules/users')
+
+router.use('/', users)
+router.use('/', home)
+router.use('/todos', todos)
+module.exports = router
