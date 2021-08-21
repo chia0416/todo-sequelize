f035afc5a0379d9a986cc3e40916bc3084d9e8ad
chia hung <a0926223478@hotmail.com>
2021-08-22 00:32:10 +0800

    feat: improved the login page

--- config/passport.js
+++ config/passport.js
@@ -17,11 +17,11 @@ module.exports = app => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
-          return done(null, false, req.flash('warning_msg', 'That email is not registered!'))
+          return done(null, false, req.flash('warning_msg', '此信箱還尚未註冊'))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, req.flash('warning_msg', 'Email or Password incorrect.'))
+            return done(null, false, req.flash('warning_msg', '信箱或密碼不正確'))
           }
           return done(null, user)
         })
